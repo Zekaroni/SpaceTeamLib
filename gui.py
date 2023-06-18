55095256e4f85248a2103d10e3c7bb3deffb176a
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-18 13:40:36 -0400

    degub pi

--- gui.py
+++ gui.py
@@ -93,15 +93,20 @@ class PiGUI:
 
         if pin+1 in self._board._active_pins.keys():
             _selected_option = self._board.pin(pin+1)._output
+            _state_var = tk.BooleanVar()
             if _selected_option:
                 output_radio = tk.Radiobutton(
                     self._main_frame, 
-                    text="Output",
+                    text="On",
+                    variable=_state_var,
+                    value=True,
                     command=lambda : self._change_state_of_pin(pin+1,True)
                 )
                 input_radio = tk.Radiobutton(
                     self._main_frame, 
-                    text="Input",
+                    text="Off",
+                    variable=_state_var,
+                    value=False,
                     command=lambda : self._change_state_of_pin(pin+1,False)
                 )
             else:
@@ -115,7 +120,7 @@ class PiGUI:
             output_button = tk.Button(
                 self._main_frame,
                 text= f"Setup pin {pin+1} for output",
-                bg = "lime green",
+                bg = "red",
                 command=lambda : self._setup_pin(pin, True)
             )
             output_button.grid(row=4, column=5, columnspan=4)
@@ -128,33 +133,6 @@ class PiGUI:
             input_button.grid(row=4, column=8, columnspan=4)
             self._active_button_config_elements.append(output_button)
             self._active_button_config_elements.append(input_button)
-
-        # self._toggle_button_state(self._active_configured_pin)
-
-
-    def _toggle_button_state(self, pin):
-        # Toggle the button state
-        if self._current_pressed_button is not None:
-            # Reset the state of the previously pressed button
-            self._button_states[self._current_pressed_button] = False
-            self._update_button_appearance(self._current_pressed_button)
-
-        self._current_pressed_button = pin
-        self._button_states[pin] = True
-        self._update_button_appearance(pin)
-
-    def _update_button_appearance(self, pin):
-        # Update the appearance of the button based on its state
-        button = self._buttons[pin]
-        if button:
-            if not self._button_states[pin]:
-                # Button is pressed
-                button.configure(bg='green')
-                self._button_states[pin] = True
-            else:
-                # Button is released
-                button.configure(bg='orange')
-                self._button_states[pin] = False
 
     def _update_font(self):
         # Calculate font size based on the available space
@@ -177,8 +155,12 @@ class PiGUI:
     
     def _change_state_of_pin(self, pinNumber, output):
         localPin = self._board.pin(pinNumber)
-        if output: localPin.turnOn()
-        else: localPin.turnOff()
+        if output:
+            localPin.turnOn()
+            self._buttons[pinNumber-1].configure(bg="lime green")
+        else:
+            localPin.turnOff()
+            self._buttons[pinNumber-1].configure(bg="red")
 
     def run(self):
         self._parent.mainloop()
