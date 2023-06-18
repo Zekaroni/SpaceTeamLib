53b73f5255298fa141ad98dc1989de91d59e0a5a
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-18 16:27:28 -0400

    debig

--- gui.py
+++ gui.py
@@ -189,7 +189,7 @@ class PiGUI:
             button_height = button.winfo_height()
 
             label_x = button.winfo_x() + button_width // 2 - self._info_box.winfo_reqwidth() // 2
-            label_y = button.winfo_y() + button_height // 2 - self._info_box.winfo_reqheight()
+            label_y = button.winfo_y() + self._info_box.winfo_reqheight()
 
             self._info_box.place(x=label_x, y=label_y)
             self._info_box.lift()
