d3d2f4839f6ad14f4c9ecbb1c345735f0a3672b2
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-11-07 14:41:55 -0500

    added motors

---
+++ motors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <pigpio.h>
+
+int main() {
+    if (gpioInitialise() < 0)
+    {
+        std::cerr << "GPIO initialization failed." << std::endl;
+        return 1;
+    }
+
+    const int servoPin = 18;
+
+    gpioSetMode(servoPin, PI_OUTPUT);
+
+    while (true) {
+        for (int angle = 0; angle <= 180; angle += 10)
+        {
+            int pulseWidth = 500 + angle * 10;
+            gpioServo(servoPin, pulseWidth);
+            time_sleep(0.5);
+        };
+        for (int angle = 180; angle >= 0; angle -= 10)
+        {
+            int pulseWidth = 500 + angle * 10;
+            gpioServo(servoPin, pulseWidth);
+            time_sleep(0.5);
+        };
+    };
+
+    gpioTerminate();
+    return 0;
+}
